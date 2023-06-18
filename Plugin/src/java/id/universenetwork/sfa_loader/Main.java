8ff466a990593dba453980bea45a7d18a9e79642
ARVIN3108 <31arvin08@gmail.com>
2023-06-18 12:24:34 +0700

    InfinityLib-Standalone Implementation

--- Plugin/src/java/id/universenetwork/sfa_loader/Main.java
+++ Plugin/src/java/id/universenetwork/sfa_loader/Main.java
@@ -1,4 +1,14 @@
 package id.universenetwork.sfa_loader;
 
-public class Main {
+import io.github.mooy1.infinitylib.core.AbstractAddon;
+public class Main extends AbstractAddon {
+    @Override
+    protected void enable() {
+
+    }
+
+    @Override
+    protected void disable() {
+
+    }
 }
