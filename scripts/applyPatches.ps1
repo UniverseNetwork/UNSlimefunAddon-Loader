9fda0375f7d05730064b1f1bd2e6ef1b254a2795
ARVIN3108 <31arvin08@gmail.com>
2023-06-22 10:36:42 +0700

    Replace Write-Host to echo

--- scripts/applyPatches.ps1
+++ scripts/applyPatches.ps1
@@ -1,10 +1,10 @@
 $basedir=(pwd)
 $gpgsign=(git config commit.gpgsign) -or "false"
-Write-Host "Rebuilding Forked projects..."
+echo "Rebuilding Forked projects..."
 
 function enableCommitSigningIfNeeded {
     if ($gpgsign -eq "true") {
-        Write-Host "Re-enabling GPG Signing"
+        echo "Re-enabling GPG Signing"
         # Yes, this has to be global
         git config --global commit.gpgsign true
     }
@@ -14,20 +14,20 @@ function applyPatch($dir) {
     $name=(Split-Path $dir -Leaf)
     Set-Location "$basedir/$dir"
 
-    Write-Host "Resetting $name..."
+    echo "Resetting $name..."
     git reset --hard HEAD
 
-    Write-Host "  Applying a patch to $name..."
+    echo "  Applying a patch to $name..."
     git am --abort >$null 2>&1
     git am --3way --ignore-whitespace "$basedir/patches/$name.patch" >$null 2>&1
     if ($LASTEXITCODE -ne 0) {
-        Write-Host "  Something did not apply cleanly to $name."
-        Write-Host "  Please review above details and finish the apply then"
-        Write-Host "  save the changes with rebuildPatches.sh"
+        echo "  Something did not apply cleanly to $name."
+        echo "  Please review above details and finish the apply then"
+        echo "  save the changes with rebuildPatches.sh"
         enableCommitSigningIfNeeded
         exit 1
     } else {
-        Write-Host "  Patches applied cleanly to $name"
+        echo "  Patches applied cleanly to $name"
     }
 }
 
@@ -35,7 +35,7 @@ function applyPatch($dir) {
 # There is also zero rational or logical reason to do so for these sub-repo AMs.
 # Calm down kids, it's re-enabled (if needed) immediately after, pass or fail.
 if ($gpgsign -eq "true") {
-    Write-Host "_Temporarily_ disabling GPG signing"
+    echo "_Temporarily_ disabling GPG signing"
     git config --global commit.gpgsign false
 }
 
