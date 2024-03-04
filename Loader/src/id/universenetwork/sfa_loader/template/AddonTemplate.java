f0c52aaaa5e19718b09de2aca6b3b146d363f96b
ARVIN3108 <70049331+ARVIN3108@users.noreply.github.com>
2024-03-04 14:18:44 +0700

    Critical fix: Fix addon configuration system that crashing the server

--- Loader/src/id/universenetwork/sfa_loader/template/AddonTemplate.java
+++ Loader/src/id/universenetwork/sfa_loader/template/AddonTemplate.java
@@ -1,6 +1,7 @@
 package id.universenetwork.sfa_loader.template;
 
 import id.universenetwork.sfa_loader.AddonsLoader;
+import id.universenetwork.sfa_loader.libraries.guizhanlib.slimefun.addon.AbstractAddon;
 import id.universenetwork.sfa_loader.libraries.guizhanlib.slimefun.addon.AddonConfig;
 import lombok.Getter;
 
@@ -9,11 +10,12 @@ import java.io.File;
 @Getter
 public abstract class AddonTemplate {
     private AddonConfig config;
-    private final String addonFolderString = "addons-config" + File.separator + AddonsLoader.getAddonName(getClass());
+    private final String addonFolderString = "addons-config/" + AddonsLoader.getAddonName(getClass());
+    private final File addonFolder = new File(AbstractAddon.getInstance().getDataFolder(), addonFolderString);
 
     public AddonTemplate() {
         try {
-            config = new AddonConfig(addonFolderString + File.separator + "config.yml");
+            config = new AddonConfig(new File(addonFolder, "config.yml"));
         } catch (Exception ignore) {
         }
     }
